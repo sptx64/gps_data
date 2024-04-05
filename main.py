b6e7bd8124a4d42412d02250832eb412cc2a3d17
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-04-05 17:12:04 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 def point_treatment(dfp, fname) :
     dfp["str"]=1
     dfp.loc[dfp["str"].isna(), "str"]=0
