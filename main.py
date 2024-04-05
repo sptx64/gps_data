5e9b836ff49d06645971e45981001f5c9bdfed73
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-04-05 17:13:17 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_github_icon = """
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 def point_treatment(dfp, fname) :
     dfp["str"]=1
