2665c0c96cd531596399b76d96d5741e07579273
sptx64 <89103497+sptx64@users.noreply.github.com>
2024-04-12 10:20:31 +1100

    Update main.py

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ if file_up :
         df
     
     df_line, df_point = df[df["Point Name"].str.startswith("Line")], df[~df["Point Name"].str.startswith("Line")]
-
+	
 	def convert_df(df):
 		return df.to_csv(index=False, header=False).encode('utf-8')
 
